b10deb067b8f8b44b9a76c8991cbf88d84681d47
simonlindstedt <simon.lindstedt@gmail.com>
2021-04-02 11:13:18 +0200

    cleanup in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,10 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterUserController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     return view('index');
-// });
 Route::view('/', 'index');
 Route::view('login', 'login')->middleware('guest');
 Route::post('login', LoginController::class);
