4ed339ec6028d16e1d0a174dafe564df5bfc11fa
simonlindstedt <simon.lindstedt@gmail.com>
2021-04-02 14:36:11 +0200

    added recipe factory and modified user

---
+++ database/factories/RecipeFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Recipe;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RecipeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Recipe::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence(),
+            'content' => implode(", ", $this->faker->paragraphs(3)),
+        ];
+    }
+}
