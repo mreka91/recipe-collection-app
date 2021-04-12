c74cb363cfc5ea0a87a58804f36f7d142fc29a33
simonlindstedt <simon.lindstedt@gmail.com>
2021-04-12 16:56:18 +0200

    bumped up the pagination

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,7 +9,7 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
-        $recipes = Recipe::select('id', 'title', 'picture_url')->orderBy('updated_at', 'desc')->paginate(5);
+        $recipes = Recipe::select('id', 'title', 'picture_url')->orderBy('updated_at', 'desc')->paginate(8);
         return view('index', [
             'recipes' => $recipes,
         ]);
